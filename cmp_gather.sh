e9038cf18f721ee0ced25cb6a8afbcb0635b2d33
bvermeulen <bruno_vermeulen2001@yahoo.com>
2024-09-29 15:45:15 +0700

    update plot gathers

--- cmp_gather.sh
+++ cmp_gather.sh
@@ -1,2 +1,2 @@
-suwind key=cdp min=$1 max=$1 < data/line_001sorted.su | suximage key=offset cmap=hsv4 perc=90 \
-   title="shot $1 after geometry" label1="Time(s)" label2="Offsset(m)"
+suwind key=cdp min=$1 max=$1 < data/line_001sorted.su | suximage key=offset d2=100 cmap=hsv0 perc=90 verbose=1 \
+   title="shot $1 after geometry" label1="Time (s)" label2="Offset (m)"
--- shot_gather.sh
+++ shot_gather.sh
@@ -1,2 +1,2 @@
-suwind key=ep min=$1 max=$1 < data/line_001.su | suximage key=offset cmap=hsv4 perc=90 \
-   title="shot $1 after geometry" label1="Time(s)" label2="Offsset(m)" 
+suwind key=ep min=$1 max=$1 < data/line_001e.su | suximage key=offset d2=25 cmap=hsv4 perc=90 \
+   title="shot $1 after geometry" label1="Time (s)" label2="Offset (m)"
